13421e8f9ee92c0156e089cfcbefcc4bbec6f69d
Bhupesh1998 <112927234+Bhupesh1998@users.noreply.github.com>
2025-05-30 15:51:42 +0530

    Add files via upload

--- React_Projects/src/App.jsx
+++ React_Projects/src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./global/Topbar";
 import Sidebar from "./global/Sidebar";
 import Dashboard from './Pages/Dashboard';
+import Team from './Pages/team';
+import Counter from './React_Projects/01_Conter/Counter';
+import TodoList from './React_Projects/02_Todo_list/TodoList';
+import ProjectsBox from './global/ProjectsBox';
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -21,6 +25,10 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/Counter" element={<Counter />}/>
+              {/* <Route path="/TodoList" element={<TodoList />}/>
+              <Route path="/team" element={<Team />}/> */}
+              <Route path="/project/:ProjectName" element={<ProjectsBox />} />
               {/* <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
